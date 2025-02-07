b303682c1df700995e722ed4e22839ecfa4f01cd
Smit Velasquez <brayanvelasquez20079@gmail.com>
2025-02-06 21:29:05 -0500

    la primera vez que se suman
    suma de personaje A y B.

---
+++ logica de programacion 1 ( operadores)  c++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+
+	cout << "ingresa dos numeros a ser sumados" << endl;
+	cin >> a >> b;
+
+	int suma;
+
+	suma = a + b;
+
+	cout << "la suma es" << suma << endl;
+	cout << "lo lograremos" << endl;
+
+	return 0;
+}
